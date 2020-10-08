5e54590ae4e295a7ff37b240b9776678bab00a0b
Andrew Stewart <1andrewstewart@gmail.com>
2020-10-08 07:55:18 -0400

    directory setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Little Mac Burger');
+INSERT INTO burgers (burger_name) VALUES ('Maui Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Possible Burger');
+
+
+
